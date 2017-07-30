e88c1a5591da709de506f4c73414fc0eca7d1407
Ramya Achutha Rao <ramyar@microsoft.com>
2017-07-29 20:08:43 -0700

    Deprecate old emmet

--- src/vs/workbench/parts/emmet/electron-browser/emmet.contribution.ts
+++ src/vs/workbench/parts/emmet/electron-browser/emmet.contribution.ts
@@ -5,32 +5,6 @@
 
 'use strict';
 
-import nls = require('vs/nls');
-
-import { Registry } from 'vs/platform/registry/common/platform';
-import { IConfigurationRegistry, Extensions as ConfigurationExtensions } from 'vs/platform/configuration/common/configurationRegistry';
-
 import './actions/expandAbbreviation';
 import './actions/wrapWithAbbreviation';
-// import './actions/base64'; // disabled - we will revisit the implementation
 
-// Configuration: emmet
-const configurationRegistry = <IConfigurationRegistry>Registry.as(ConfigurationExtensions.Configuration);
-configurationRegistry.registerConfiguration({
-	'id': 'emmet',
-	'order': 6,
-	'title': nls.localize('emmetConfigurationTitle', "Emmet"),
-	'type': 'object',
-	'properties': {
-		'emmet.triggerExpansionOnTab': {
-			'type': 'boolean',
-			'default': true,
-			'description': nls.localize('triggerExpansionOnTab', "When enabled, emmet abbreviations are expanded when pressing TAB. Not applicable when emmet.useNewemmet is set to true.")
-		},
-		'emmet.preferences': {
-			'type': 'object',
-			'default': {},
-			'description': nls.localize('emmetPreferences', "Preferences used to modify behavior of some actions and resolvers of Emmet. Not applicable when emmet.useNewemmet is set to true.")
-		}
-	}
-});
