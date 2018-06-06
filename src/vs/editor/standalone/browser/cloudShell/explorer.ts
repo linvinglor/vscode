7c86c599bc2178a39190b909370d128e430413bb
Christof Marti <chrmarti@microsoft.com>
2018-06-06 16:18:48 +0200

    New File / Folder

--- src/vs/editor/standalone/browser/cloudShell/explorer.ts
+++ src/vs/editor/standalone/browser/cloudShell/explorer.ts
@@ -7,15 +7,21 @@
 import { Tree } from 'vs/base/parts/tree/browser/treeImpl';
 import { IDataSource, IRenderer, IController, ISorter } from 'vs/base/parts/tree/browser/tree';
 import { TPromise } from 'vs/base/common/winjs.base';
+import { Color } from 'vs/base/common/color';
 
 interface IEntry {
 	uri: string;
 	name: string;
 	isFolder: boolean;
+	isEditable?: boolean;
 	parent?: IEntry;
 }
 
 class DataSource implements IDataSource {
+
+	newEntryParentUri: string;
+	newEntryIsFolder = false;
+
 	getId(tree, element) {
 		return element.uri;
 	}
@@ -29,6 +35,14 @@ class DataSource implements IDataSource {
 					if (res.status >= 200 && res.status <= 299) {
 						return res.json()
 							.then((entries: IEntry[]) => {
+								if (this.newEntryParentUri && element.uri === this.newEntryParentUri) {
+									entries.push({
+										uri: element.uri + '?newentry',
+										name: '',
+										isFolder: this.newEntryIsFolder,
+										isEditable: true
+									});
+								}
 								entries.forEach(entry => entry.parent = element);
 								return entries;
 							});
@@ -45,22 +59,74 @@ class DataSource implements IDataSource {
 
 class Renderer implements IRenderer {
 
+	private static TEMPLATE_ID = 'explorer_entry';
+	private static EDITABLE_TEMPLATE_ID = 'editable_explorer_entry';
+
+	constructor(private dataSource: DataSource) { }
+
 	getHeight(tree, element) {
 		return 22;
 	}
 	getTemplateId(tree, element) {
-		return 'explorer_entry';
+		return element.isEditable ? Renderer.EDITABLE_TEMPLATE_ID : Renderer.TEMPLATE_ID;
 	}
 
 	renderTemplate(tree, templateId, container) {
-		const span = document.createElement('span');
-		span.classList.add('explorer-entry');
-		container.appendChild(span);
-		return span;
+		if (templateId === Renderer.TEMPLATE_ID) {
+			const span = document.createElement('span');
+			span.classList.add('explorer-entry');
+			container.appendChild(span);
+			return { span };
+		}
+		if (templateId === Renderer.EDITABLE_TEMPLATE_ID) {
+			const input = document.createElement('input');
+			input.type = 'text';
+			input.classList.add('explorer-entry', 'editable');
+			container.appendChild(input);
+			const data: { input: HTMLInputElement, element?: IEntry } = { input };
+			input.addEventListener('keydown', ev => {
+				if (ev.keyCode === 13 && input.value) {
+					input.disabled = true;
+					const name = input.value;
+					const uri = `${data.element.parent.uri}/${name}`;
+					fetch(consoleUri + uri, {
+						headers: { 'Content-Type': 'application/json' },
+						body: JSON.stringify({ isFolder: data.element.isFolder }),
+						method: 'POST'
+					})
+						.catch(console.error)
+						.then(() => {
+							this.dataSource.newEntryParentUri = null;
+							tree.refresh(data.element.parent);
+							if (!data.element.isFolder) {
+								openEditor(uri, name);
+							}
+						});
+				} else if (ev.keyCode === 27) {
+					this.dataSource.newEntryParentUri = null;
+					tree.refresh(data.element.parent);
+				}
+			});
+			input.addEventListener('blur', ev => {
+				if (!input.disabled && this.dataSource.newEntryParentUri) {
+					this.dataSource.newEntryParentUri = null;
+					tree.refresh(data.element.parent);
+				}
+			});
+			return data;
+		}
+		throw new Error('Unknown template id');
 	}
 
 	renderElement(tree, element, templateId, templateData) {
-		templateData.textContent = element.name;
+		if (templateId === Renderer.TEMPLATE_ID) {
+			templateData.span.textContent = element.name;
+		} else if (templateId === Renderer.EDITABLE_TEMPLATE_ID) {
+			templateData.input.disabled = false;
+			templateData.input.value = element.name;
+			templateData.element = element;
+			templateData.input.focus();
+		}
 	}
 
 	disposeTemplate(tree, templateId, templateData) {
@@ -70,19 +136,15 @@ class Renderer implements IRenderer {
 class Controller implements IController {
 
 	onClick(tree, element, event) {
+		if (element.isEditable) {
+			return true;
+		}
+		tree.setSelection([element]);
 		if (element.isFolder) {
 			tree.toggleExpansion(element);
 			return true;
 		}
-		document.dispatchEvent(new CustomEvent('show', {
-			detail: {
-				component: 'editor',
-				arguments: {
-					fileUri: element.uri,
-					fileName: element.name
-				}
-			}
-		}));
+		openEditor(element.uri, element.name, true);
 		return true;
 	}
 
@@ -116,12 +178,29 @@ class Sorter implements ISorter {
 	}
 }
 
+function openEditor(fileUri: string, fileName: string, preserveFocus?: boolean) {
+	document.dispatchEvent(new CustomEvent('show', {
+		detail: {
+			component: 'editor',
+			arguments: {
+				fileUri,
+				fileName,
+				preserveFocus
+			}
+		}
+	}));
+}
+
+const dataSource = new DataSource();
+
 const tree = new Tree(document.getElementById('editor-explorer'), {
-	dataSource: new DataSource(),
-	renderer: new Renderer(),
+	dataSource,
+	renderer: new Renderer(dataSource),
 	controller: new Controller(),
 	sorter: new Sorter()
-});
+}, {
+		listActiveSelectionBackground: Color.fromHex('#094771')
+	});
 
 export function show(folderUri: string) {
 	document.getElementById('editor-explorer')
@@ -134,6 +213,25 @@ export function show(folderUri: string) {
 		};
 		tree.setInput(root);
 	}
+}
+
+export function newFile() {
+	newEntry(false);
+}
+
+export function newFolder() {
+	newEntry(true);
+}
+
+function newEntry(isFolder: boolean) {
+	const selected = tree.getSelection()[0];
+	const parent = selected ? (selected.isFolder ? selected : selected.parent) : tree.getInput();
+	dataSource.newEntryParentUri = parent.uri;
+	dataSource.newEntryIsFolder = isFolder;
+	if (parent && !tree.isExpanded(parent)) {
+		tree.expand(parent);
+	}
+	tree.refresh(parent);
 }
 
 export function refresh() {
